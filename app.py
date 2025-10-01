e0b4c112ac17f61a28fb246a87e0c489fb503fde
Alok Gautam <23f2003251@ds.study.iitm.ac.in>
2025-10-01 19:33:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 from flask import Flask
 from backend.models import db
 
-app=None
-def setup_app():
-    app=Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///ticket_show.sqlite3"
-    db.init_app(app)
-    app.app_context().push()
-    # app.debug=True
-    print("Quiz master app is starting.....")
+# Create Flask app
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ticket_show.sqlite3"
 
-setup_app()
+# Initialize DB
+db.init_app(app)
+app.app_context().push()
 
+print("Quiz master app is starting.....")
 
+# Import controllers after app is created
 from backend.controller import *
 
+# Vercel will look for `app`
 if __name__ == "__main__":
     app.run()
+
